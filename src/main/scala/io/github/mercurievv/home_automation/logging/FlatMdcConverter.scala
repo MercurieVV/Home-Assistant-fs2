64a85a8155bdab6fc6eaa51cad36e85ebbc0225a
MercurieVV <mercurievv@gmail.com>
2026-03-25 19:03:14 +0200

    MDC logging have more values

---
+++ src/main/scala/io/github/mercurievv/home_automation/logging/FlatMdcConverter.scala
@@ -0,0 +1,18 @@
+package io.github.mercurievv.home_automation.logging
+
+import scala.jdk.CollectionConverters.*
+
+import ch.qos.logback.classic.pattern.ClassicConverter
+import ch.qos.logback.classic.spi.ILoggingEvent
+
+/** Outputs all non-empty, non-JSON MDC entries as ",key=value,key=value" (leading comma when non-empty). */
+class FlatMdcConverter extends ClassicConverter {
+
+  override def convert(event: ILoggingEvent): String = {
+    val entries = event.getMDCPropertyMap.asScala
+      .filter { case (_, v) => v != null && v.nonEmpty && !v.startsWith("{") && !v.startsWith("[") }
+      .map { case (k, v) => s"$k=$v" }
+      .mkString(",")
+    if entries.isEmpty then "" else "," + entries
+  }
+}
